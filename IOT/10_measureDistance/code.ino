d2a59fe13db2dabc1c29f8d75b50a95542413547
Shaan Subbaiah <shaansubbaiah@yahoo.com>
2020-10-07 19:56:28 +0530

    Add lab3 IOT files

---
+++ IOT/10_measureDistance/code.ino
@@ -0,0 +1,51 @@
+// Shaan Subbaiah B C - 1BM18CS096
+// Distanceusing HC-SR04
+
+/*
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+*/
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Distance:");
+  
+  pinMode(7, INPUT);
+  pinMode(8, OUTPUT);
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  
+  digitalWrite(8, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(8, LOW);
+  
+  float duration = pulseIn(7, HIGH);
+  float dist = duration*0.034/2;
+  
+  lcd.print((String)dist+"cm");
+  Serial.println((String)dist+"cm");
+}
+ 
