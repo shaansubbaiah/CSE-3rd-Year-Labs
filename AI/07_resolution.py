3c65de1ac9125ee11db2099f3223a31314e9ecc5
Shaan Subbaiah <shaansubbaiah@yahoo.com>
2020-12-17 14:00:58 +0530

    Add socket programming files

--- AI/07_resolution.py
+++ AI/07_resolution.py
@@ -1,367 +1,108 @@
- # Global variable kb (knowledge base)
-kb = []
+import re
 
-# Reset kb to an empty list
-def CLEAR():
-    global kb
-    kb = []
 
-# Insert sentence to the kb
-def TELL(sentence):
-    global kb
-    # If the sentence is a clause, insert directly.
-    if isClause(sentence):
-        kb.append(sentence)
-    # If not, convert to CNF, and then insert clauses one by one.
-    else:
-        sentenceCNF = convertCNF(sentence)
-        if not sentenceCNF:
-            print("Illegal input")
-            return
-        # Insert clauses one by one when there are multiple clauses
-        if isAndList(sentenceCNF):
-            for s in sentenceCNF[1:]:
-                kb.append(s)
-        else:
-            kb.append(sentenceCNF)
+def negate(term):
+    return f"~{term}" if term[0] != "~" else term[1]
 
-# 'ASK' the kb whether a sentence is True or not
-def ASK(sentence):
-    global kb
 
-    # Negate the sentence, and convert it to CNF accordingly.
-    if isClause(sentence):
-        neg = negation(sentence)
-    else:
-        sentenceCNF = convertCNF(sentence)
-        if not sentenceCNF:
-            print("Illegal input")
-            return
-        neg = convertCNF(negation(sentenceCNF))
+def reverse(clause):
+    if len(clause) > 2:
+        t = split_terms(clause)
+        return f"{t[1]}v{t[0]}"
+    return ""
 
-    # Insert individual clauses that we need to ask to ask_list.
-    ask_list = []
-    if isAndList(neg):
-        for n in neg[1:]:
-            nCNF = makeCNF(n)
-            if type(nCNF).__name__ == 'list':
-                ask_list.insert(0, nCNF)
-            else:
-                ask_list.insert(0, nCNF)
-    else:
-        ask_list = [neg]
-# Create a new list combining the asked sentence and kb.
-    # Resolution will happen between the items in the list.
-    clauses = ask_list + kb[:]
 
-    # Recursivly conduct resoltion between items in the clauses list
-    # until it produces an empty list or there's no more pregress.
-    while True:
-        new_clauses = []
-        for c1 in clauses:
-            for c2 in clauses:
-                if c1 is not c2:
-                    resolved = resolve(c1, c2)
-                    if resolved == False:
-                        continue
-                    if resolved == []:
-                        return True
-                    new_clauses.append(resolved)
+def split_terms(rule):
+    exp = "(~*[PQRS])"
+    terms = re.findall(exp, rule)
+    return terms
 
-        if len(new_clauses) == 0:
-            return False
 
-        new_in_clauses = True
-        for n in new_clauses:
-            if n not in clauses:
-                new_in_clauses = False
-                clauses.append(n)
+def contradiction(query, clause):
+    contradictions = [f"{query}v{negate(query)}", f"{negate(query)}v{query}"]
+    return clause in contradictions or reverse(clause) in contradictions
 
-        if new_in_clauses:
-            return False
-    return False
 
-# Conduct resolution on two CNF clauses.
-def resolve(arg_one, arg_two):
-    resolved = False
-
-    s1 = make_sentence(arg_one)
-    s2 = make_sentence(arg_two)
-
-    resolve_s1 = None
-    resolve_s2 = None
-
-    # Two for loops that iterate through the two clauses.
-    for i in s1:
-        if isNotList(i):
-            a1 = i[1]
-            a1_not = True
-        else:
-            a1 = i
-            a1_not = False
-
-        for j in s2:
-            if isNotList(j):
-                a2 = j[1]
-                a2_not = True
-            else:
-                a2 = j
-                a2_not = False
-
-            # cancel out two literals such as 'a' $ ['not', 'a']
-            if a1 == a2:
-                if a1_not != a2_not:
-                    # Return False if resolution already happend
-                    # but contradiction still exists.
-                    if resolved:
-                        return False
+def resolve(kb, query):
+    temp = kb.copy()
+    temp += [negate(query)]
+    steps = dict()
+    for rule in temp:
+        steps[rule] = "Given."
+    steps[negate(query)] = "Negated conclusion."
+    i = 0
+    while i < len(temp):
+        n = len(temp)
+        j = (i + 1) % n
+        clauses = []
+        while j != i:
+            terms1 = split_terms(temp[i])
+            terms2 = split_terms(temp[j])
+            for c in terms1:
+                if negate(c) in terms2:
+                    t1 = [t for t in terms1 if t != c]
+                    t2 = [t for t in terms2 if t != negate(c)]
+                    gen = t1 + t2
+                    if len(gen) == 2:
+                        if gen[0] != negate(gen[1]):
+                            clauses += [f"{gen[0]}v{gen[1]}"]
+                        else:
+                            if contradiction(query, f"{gen[0]}v{gen[1]}"):
+                                temp.append(f"{gen[0]}v{gen[1]}")
+                                steps[
+                                    ""
+                                ] = f"Resolved {temp[i]} and {temp[j]} to {temp[-1]}, which is in turn null. \
+                                \nA contradiction is found when {negate(query)} is assumed as true. Hence, {query} is true."
+                                return steps
+                    elif len(gen) == 1:
+                        clauses += [f"{gen[0]}"]
                     else:
-                        resolved = True
-                        resolve_s1 = i
-                        resolve_s2 = j
-                        break
-                    # Return False if not resolution happened
-    if not resolved:
-        return False
-
-    # Remove the literals that are canceled
-    s1.remove(resolve_s1)
-    s2.remove(resolve_s2)
-
-    # # Remove duplicates
-    result = clear_duplicate(s1 + s2)
-
-    # Format the result.
-    if len(result) == 1:
-        return result[0]
-    elif len(result) > 1:
-        result.insert(0, 'or')
-
-    return result
-
-# Prepare sentences for resolution.
-def make_sentence(arg):
-    if isLiteral(arg) or isNotList(arg):
-        return [arg]
-    if isOrList(arg):
-        return clear_duplicate(arg[1:])
-    return
-
-# Clear out duplicates in a sentence.
-def clear_duplicate(arg):
-    result = []
-    for i in range(0, len(arg)):
-        if arg[i] not in arg[i+1:]:
-            result.append(arg[i])
-    return result
-
-# Check whether a sentence is a legal CNF clause.
-def isClause(sentence):
-    if isLiteral(sentence):
-        return True
-    if isNotList(sentence):
-        if isLiteral(sentence[1]):
-            return True
-        else:
-            return False
-    if isOrList(sentence):
-        for i in range(1, len(sentence)):
-            if len(sentence[i]) > 2:
-                return False
-            elif not isClause(sentence[i]):
-                return False
-        return True
-    return False
-
-# Check if a sentence is a legal CNF.
-def isCNF(sentence):
-    if isClause(sentence):
-        return True
-    elif isAndList(sentence):
-        for s in sentence[1:]:
-            if not isClause(s):
-                return False
-        return True
-    return False
-
-# Negate a sentence.
-def negation(sentence):
-    if isLiteral(sentence):
-        return ['not', sentence]
-    if isNotList(sentence):
-        return sentence[1]
-
-    # DeMorgan:
-    if isAndList(sentence):
-        result = ['or']
-        for i in sentence[1:]:
-            if isNotList(sentence):
-                result.append(i[1])
-            else:
-                result.append(['not', sentence])
-        return result
-    if isOrList(sentence):
-        result = ['and']
-        for i in sentence[:]:
-            if isNotList(sentence):
-                result.append(i[1])
-            else:
-                result.append(['not', i])
-        return result
-    return None
+                        if contradiction(query, f"{terms1[0]}v{terms2[0]}"):
+                            temp.append(f"{terms1[0]}v{terms2[0]}")
+                            steps[
+                                ""
+                            ] = f"Resolved {temp[i]} and {temp[j]} to {temp[-1]}, which is in turn null. \
+                            \nA contradiction is found when {negate(query)} is assumed as true. Hence, {query} is true."
+                            return steps
+            for clause in clauses:
+                if (
+                    clause not in temp
+                    and clause != reverse(clause)
+                    and reverse(clause) not in temp
+                ):
+                    temp.append(clause)
+                    steps[clause] = f"Resolved from {temp[i]} and {temp[j]}."
+            j = (j + 1) % n
+        i += 1
+    return steps
 
 
-# Convert a sentence into CNF.
-def convertCNF(sentence):
-    while not isCNF(sentence):
-        if sentence is None:
-            return None
-        sentence = makeCNF(sentence)
-    return sentence
-
-# Help make a sentence into CNF.
-def makeCNF(sentence):
-    if isLiteral(sentence):
-        return sentence
-
-    if (type(sentence).__name__ == 'list'):
-        operand = sentence[0]
-        if isNotList(sentence):
-            if isLiteral(sentence[1]):
-                return sentence
-            cnf = makeCNF(sentence[1])
-            if cnf[0] == 'not':
-                return makeCNF(cnf[1])
-            if cnf[0] == 'or':
-                result = ['and']
-                for i in range(1, len(cnf)):
-                    result.append(makeCNF(['not', cnf[i]]))
-                return result
-            if cnf[0] == 'and':
-                result = ['or']
-                for i in range(1, len(cnf)):
-                    result.append(makeCNF(['not', cnf[i]]))
-                return result
-            return "False: not"
-
-        # Implication Elimination:
-        if operand == 'implies' and len(sentence) == 3:
-            return makeCNF(['or', ['not', makeCNF(sentence[1])], makeCNF(sentence[2])])
-            # Biconditional Elimination:
-        if operand == 'biconditional' and len(sentence) == 3:
-            s1 = makeCNF(['implies', sentence[1], sentence[2]])
-            s2 = makeCNF(['implies', sentence[2], sentence[1]])
-            return makeCNF(['and', s1, s2])
-
-        if isAndList(sentence):
-            result = ['and']
-            for i in range(1, len(sentence)):
-                cnf = makeCNF(sentence[i])
-                # Distributivity:
-                if isAndList(cnf):
-                    for i in range(1, len(cnf)):
-                        result.append(makeCNF(cnf[i]))
-                    continue
-                result.append(makeCNF(cnf))
-            return result
-
-        if isOrList(sentence):
-            result1 = ['or']
-            for i in range(1, len(sentence)):
-                cnf = makeCNF(sentence[i])
-                # Distributivity:
-                if isOrList(cnf):
-                    for i in range(1, len(cnf)):
-                        result1.append(makeCNF(cnf[i]))
-                    continue
-                result1.append(makeCNF(cnf))
-                # Associativity:
-            while True:
-                result2 = ['and']
-                and_clause = None
-                for r in result1:
-                    if isAndList(r):
-                        and_clause = r
-                        break
-
-                # Finish when there's no more 'and' lists
-                # inside of 'or' lists
-                if not and_clause:
-                    return result1
-
-                result1.remove(and_clause)
-
-                for i in range(1, len(and_clause)):
-                    temp = ['or', and_clause[i]]
-                    for o in result1[1:]:
-                        temp.append(makeCNF(o))
-                        result2.append(makeCNF(temp))
-                result1 = makeCNF(result2)
-            return None
-    return None
-
-# Below are 4 functions that check the type of a variable
-def isLiteral(item):
-    if type(item).__name__ == 'str':
-        return True
-    return False
+def resolution(kb, query):
+    kb = kb.split(" ")
+    steps = resolve(kb, query)
+    print("\nStep\t|Clause\t|Derivation\t")
+    print("-" * 30)
+    i = 1
+    for step in steps:
+        print(f" {i}.\t| {step}\t| {steps[step]}\t")
+        i += 1
 
 
-def isNotList(item):
-    if type(item).__name__ == 'list':
-        if len(item) == 2:
-            if item[0] == 'not':
-                return True
-    return False
+def main():
+    print("Enter the kb:")
+    kb = input()
+    print("Enter the query:")
+    query = input()
+    resolution(kb, query)
 
 
-def isAndList(item):
-    if type(item).__name__ == 'list':
-        if len(item) > 2:
-            if item[0] == 'and':
-                return True
-    return False
+# test 1
+# (P^Q)<=>R : (Rv~P)v(Rv~Q)^(~RvP)^(~RvQ)
+# main()
+# test 2
+# (P=>Q)=>Q, (P=>P)=>R, (R=>S)=>~(S=>Q)
+main()
 
-
-def isOrList(item):
-    if type(item).__name__ == 'list':
-        if len(item) > 2:
-            if item[0] == 'or':
-                return True
-    return False
-
-if __name__ == "__main__":
-    CLEAR()
-
-    print("Test 1")
-    TELL(['implies', 'p', 'q'])
-    TELL(['implies', 'r', 's'])
-    ASK(['implies',['or','p','r'], ['or', 'q', 's']])
-
-    CLEAR()
-
-    print("Test 2")
-    TELL('p')
-    TELL(['implies',['and','p','q'],'r'])
-    TELL(['implies',['or','s','t'],'q'])
-    TELL('t')
-    ASK('r')
-
-    CLEAR()
-
-    print("Test 3")
-    TELL('a')
-    TELL('b')
-    TELL('c')
-    TELL('d')
-    ASK(['or', 'a', 'b', 'c', 'd'])
-
-    CLEAR()
-
-    print("Test 4")
-    TELL('a')
-    TELL('b')
-    TELL(['or', ['not', 'a'], 'b'])
-    TELL(['or', 'c', 'd'])
-    TELL('d')
-    ASK('c')
+# sample input
+# PvQ PvR ~PvR RvS Rv~Q ~Sv~Q
+# R
